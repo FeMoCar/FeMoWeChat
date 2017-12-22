08510d50c10b2b7618da4d08d72ed2a5efdb66b6
yyw <15058046181_d@sina.com>
2017-12-22 17:43:28 +0800

    对接商品二级分类和商品分类型号
    对接商品二级分类和商品分类型号

--- femoProject/js/common.js
+++ femoProject/js/common.js
@@ -1,28 +1,99 @@
 /**
-共用功能类
-调用方法
-var Comm = Common();
-Comm.Func();
-**/
+ 共用功能类
+ 调用方法
+ var Comm = Common();
+ Comm.Func();
+ **/
 function Common() {
-	var common = {};
-	//ajax封装
-	common.ajax = function(_url, paramData, Type, callBack) {
-		$.ajax({
-			type: Type,
-			url: _url,
-			dataType: "json",
-			data: paramData
-			success: function(data) {
-				callBack(data);
-			},
-			complete: function() {
+    var common = {};
+    //ajax封装
+    common.ajax = function (_url, Type, paramData, callBack) {
+        $.ajax({
+            type: Type,
+            url: _url,
+            dataType: "json",
+            data: paramData,
+            success: function (ret) {
+                callBack(ret);
+            },
+            complete: function () {
 
-			}
-		});
-	}
-	common.ccc = function(tt) {
-		alert(tt);
-	}
-	return common;
+            }
+        });
+    }
+    //获取商品分类数据
+    common.getProductClass=function ($scope) {
+        var startRow = 1;
+        var endRow = 50;
+        var getProductClassUrl = Config.getProductClassUrl + "startrow=" + startRow + "&endrow=" + endRow;
+        //alert(getProductClassUrl);
+        common.ajax(getProductClassUrl, "get", "", function (res) {
+            //alert(JSON.stringify(res));
+            $scope.productClassData = res.list;
+            $scope.$apply();
+        });
+    }
+    //获取商品二级分类数据
+    common.getProductSecondClass=function ($scope) {
+        var startRow = 1;
+        var endRow = 50;
+        var getProductSecondClassUrl = Config.getProductSecondClassUrl + "startrow=" + startRow + "&endrow=" + endRow;
+        common.ajax(getProductSecondClassUrl, "get", "", function (res) {
+            //alert(JSON.stringify(res));
+            $scope.productSecondClassData = res.list;
+            $scope.$apply();
+        });
+    }
+    //图片预览及上传
+    common.uploadImg=function (fileDom, previews, imageForms) {
+        //判断是否支持FileReader
+        if (window.FileReader) {
+            var reader = new FileReader();
+        } else {
+            alert("您的设备不支持图片预览功能，如需该功能请升级您的设备！");
+        }
+
+        //获取文件
+        var file = fileDom.files[0];
+        var imageType = /^image\//;
+        //是否是图片
+        if (!imageType.test(file.type)) {
+            alert("请选择图片！");
+            return;
+        }
+        //读取完成
+        reader.onload = function (e) {
+            //获取图片dom
+            //var img = document.getElementById("preview");
+            //图片路径设置为读取的图片
+            //img.src = e.target.result;
+            $("#" + previews).attr('src', e.target.result);
+        };
+
+        reader.readAsDataURL(file);
+
+        //上传图片到服务器
+        var ajax_option = {
+            url: Config.uploadImg,
+            type: "post",
+            success: function (responseText) {
+                //console.log(responseText);
+                //alert(responseText);
+                //addModalClassImg = responseText;
+                addModalClassImg = responseText.replace("\"", "").replace("\"", ""); //去掉双引号
+                //alert(addModalClassImg);
+            },
+            beforSubmit: function (formData, jqForm, options) {
+                //提交之前的回调函数。参数详解：formData，数组对象，为表单的内容；jqForm，jQuery对象，
+                //封装了表单的元素；options，之前设置的ajaxSubmit的option对象。
+                //console.log(formData);
+            },
+        };
+        $("#" + imageForms).ajaxSubmit(ajax_option);
+        return false;
+    }
+    common.cc = function (tt) {
+        alert(tt);
+    }
+    return common;
 }
--- femoProject/js/config.js
+++ femoProject/js/config.js
@@ -3,25 +3,33 @@
  共用功能类
  调用方法
  var Config = Config();
- Config.Func();  
+ Config.Func();
  */
 var host = "http://39.104.15.177:8080/FeMoDemo/";
 var hostPath = "http://39.104.15.177:8080/FeMoDemo";
 
 function Config() {
-	var config = {
-		"getUserInfoUrl": host + "WxUser/selectall?", //后台查询所有用户信息接口
-		"getUserInfoSearchUrl": host + "WxUser/selectuser?", //微信查询用户信息接口
-		"getModalClassUrl": host + "Classification/selectClassification?", //后台查询模块分类信息
-		"getAddModalClassUrl": host + "Classification/addClassification?", //后台添加模块分类信息
-		"getUpdateModalClassUrl": host + "Classification/updateClassification?", //后台更新模块分类信息
-		"uploadImg": host + "FileUpload/SaveFile", //图片上传接口
-		"getProductClassUrl": host + "ProductClass/selectAll?", //获取商品分类
-		"getUpdateProductClassUrl": host + "ProductClass/updateProductClass?", //更新商品分类
-		"getAddProductClassUrl": host + "ProductClass/addProductClass?", //添加商品分类
+    var config = {
+        "getUserInfoUrl": host + "WxUser/selectall?", //后台查询所有用户信息接口
+        "getUserInfoSearchUrl": host + "WxUser/selectuser?", //微信查询用户信息接口
+        "getModalClassUrl": host + "Classification/selectClassification?", //后台查询模块分类信息
+        "getAddModalClassUrl": host + "Classification/addClassification?", //后台添加模块分类信息
+        "getUpdateModalClassUrl": host + "Classification/updateClassification", //后台更新模块分类信息
 
-	}
-	return config;
+        "uploadImg": host + "FileUpload/SaveFile", //图片上传接口
+        "getProductClassUrl": host + "ProductClass/selectAll?", //获取商品分类
+        "UpdateProductClassUrl": host + "ProductClass/updateProductClass", //更新商品分类
+        "getAddProductClassUrl": host + "ProductClass/addProductClass?", //添加商品分类
+        "getProductSecondClassUrl": host + "ProductSecondClass/selectProductSecondclass?",//获取商品二级分类
+
+        "addProductSecondClassUrl": host + "ProductSecondClass/addProductSecondclass",//添加商品二级分类
+        "updateProductSecondClassUrl": host + "ProductSecondClass/updateProductSecondclass",//更新商品二级分类
+        "getProductClassTypeUrl": host + "ProductSpec/selectAll?",//获取商品分类型号
+        "addProductClassTypeUrl": host + "ProductSpec/addProductSpec",//添加商品二级分类
+        "updateProductClassTypeUrl": host + "ProductSpec/updateProductSpec",//更新商品二级分类
+
+    }
+    return config;
 }
 
  
